5516bb29c9a19f4f9ca0c01c6c1c441705bb67e8
felipemartins8058 <felipecosmemartins10@gmail.com>
2022-01-18 23:03:31 -0300

    Initializing the App

---
+++ android/app/src/main/kotlin/com/example/poc_sensors/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poc_sensors
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,161 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LocationApp(),
+    );
+  }
+}
+
+class LocationApp extends StatefulWidget {
+  const LocationApp({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _LocationAppState createState() => _LocationAppState();
+}
+
+class _LocationAppState extends State<LocationApp> {
+  List<double>? _accelerometerValues;
+  List<double>? _userAccelerometerValues;
+  List<double>? _gyroscopeValues;
+  List<double>? _magnetometerValues;
+  final _streamSubscriptions = <dynamic>[];
+
+  var locationMessage = "";
+
+  void getCurrentLocation() async {
+    var position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    var lastPosition = await Geolocator.getLastKnownPosition();
+    print(lastPosition);
+    var lat = position.latitude;
+    var long = position.longitude;
+    print("$lat, $long");
+
+    setState(() {
+      locationMessage = "$position";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final accelerometer =
+        _accelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final gyroscope =
+        _gyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final userAccelerometer = _userAccelerometerValues
+        ?.map((double v) => v.toStringAsFixed(1))
+        .toList();
+    final magnetometer =
+        _magnetometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GPS"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.location_on,
+              size: 46.0,
+              color: Colors.blue,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Get User Location",
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text("Position: $locationMessage"),
+            ElevatedButton(
+              onPressed: () {
+                getCurrentLocation();
+              },
+              child: Text("Get current location",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            Text('Accelerometer: $accelerometer'),
+            Text('UserAccelerometer: $userAccelerometer'),
+            Text('Gyroscope: $gyroscope'),
+            Text('Magnetometer: $magnetometer')
+          ],
+        ),
+      ),
+        
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (final subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions.add(
+      accelerometerEvents.listen(
+        (AccelerometerEvent event) {
+          setState(() {
+            _accelerometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      gyroscopeEvents.listen(
+        (GyroscopeEvent event) {
+          setState(() {
+            _gyroscopeValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      userAccelerometerEvents.listen(
+        (UserAccelerometerEvent event) {
+          setState(() {
+            _userAccelerometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      magnetometerEvents.listen(
+        (MagnetometerEvent event) {
+          setState(() {
+            _magnetometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+  }
+}
